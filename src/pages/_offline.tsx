a961c06c781ccb48aac06d376821afc05b61c4ea
C. Spencer Beggs <spencer@beg.gs>
2022-05-27 21:32:35 -0400

    stash

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,9 @@
+import { DefaultLayout } from "@components/layouts/default";
+
+export default function Custom404() {
+	return (
+		<DefaultLayout>
+			<h1>You are offline</h1>
+		</DefaultLayout>
+	);
+}
