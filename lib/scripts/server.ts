8ab006b90baf49815ee42dd98504c4d92066ea91
C. Spencer Beggs <spencer@beg.gs>
2022-12-13 21:19:26 -0500

    Stash

--- lib/scripts/server.ts
+++ lib/scripts/server.ts
@@ -1,14 +1,10 @@
 import "./config.js";
-import { Socket } from "dgram";
-import { IncomingMessage, request, ServerResponse } from "http";
-import { connect, constants, createSecureServer, Http2ServerRequest, Http2ServerResponse } from "http2";
+import { IncomingMessage, ServerResponse } from "http";
+import { createSecureServer, Http2ServerRequest } from "http2";
 import { hostname } from "os";
 import process from "process";
 import chalk from "chalk";
-import { head } from "lodash-es";
 import next from "next";
-import websocket from "websocket-stream";
-import { Server, WebSocketServer } from "ws";
 import { cjsHack } from "../helpers/cjs-hack.js";
 import { log } from "../helpers/deep-log.js";
 import { sslCredentials } from "../helpers/pem.js";
@@ -23,7 +19,7 @@ if (process.env.APP_ENV === "local") {
 // @ts-ignore
 const { credentials } = await sslCredentials({
 	name: "next",
-	commonName: site.host,
+	commonName: site.origin,
 	folder: new URL("../../.ssl/", import.meta.url),
 	altNames: [`${hostname()}:${port}`]
 });
@@ -81,91 +77,16 @@ const server = createSecureServer(
 	}
 );
 
-// const wss = createSecureServer({
-// 	...credentials,
-// 	allowHTTP1: false,
-// 	settings: {
-// 		enableConnectProtocol: true
-// 	}
-// });
-
-// wss.on("stream", (stream, headers) => {
-// 	if (headers[":method"] !== "CONNECT") {
-// 		// Only accept CONNECT requests
-// 		stream.close(constants.NGHTTP2_REFUSED_STREAM);
-// 		return;
-// 	}
-// });
-
-// wss.on("connect", (socket) => {
-// 	console.log("upgrade", socket.localAddress);
-// });
-
-// wss.listen(3000, hostname(), () => {
-// 	console.log("Listening on port 3000");
-// });
-
-// server.on("stream", (stream, headers) => {
-// 	if (headers[":method"] !== "CONNECT") {
-// 		// Only accept CONNECT requests
-// 		stream.close(constants.NGHTTP2_REFUSED_STREAM);
-// 		return;
-// 	}
-// });
-
-	//const client = new WebSocketServer({ port: 3001});
-
-
-server.on("connection", (socket: Socket) => {
-	//const { port, addresss } = socket.address();
-	socket.send("bar");
-	//console.log(client);
-	// client.handleUpgrade(request, socket, head, function done(ws) {
-	// 	client.emit("connection", ws, request);
-	// });
-	// const { port, address: host } = socket.address();
-	// client.addListener("connection", (ws, req) => {
-	// 	console.log(ws);
-	// 	console.log(req);
-
-	// });
-	// client.on("message", (data) => {
-	// 	console.log(data);
-	// 	socket.emit(data.toString());
-	// });
-});
-
-// server.on("stream", (stream, headers) => {
-// 	console.log("FFFF");
-// 	if (headers[":method"] === "CONNECT") {
-// 		stream.respond({
-// 			"sec-websocket-protocol": headers["sec-websocket-protocol"]
-// 		});
-// 		const wss = websocket.createServer({ noServer: true });
-// 		// @ts-ignore
-// 		console.log(wss);
-// 		// @ts-ignore
-// 		wss.on("connection", function connection(ws, request, client) {
-// 			// @ts-ignore
-// 			ws.on("message", function message(data) {
-// 				console.log(`Received message ${data} from user ${client}`);
-// 			});
-// 		});
-// 	} else {
-// 		// If it's any other method, just respond with 200 and "ok"
-// 		stream.respond();
-// 		stream.end("ok\n");
-// 	}
-// });
-
 // configure Next.js
 // @ts-ignore
 const app = next({
 	dev: true,
 	port,
-	isNextDevCommand: false,
+	isNextDevCommand: true,
 	hostname: hostname(),
-	customServer: true
+	customServer: true,
+	// @ts-ignore
+	httpServer: server
 });
 
 // @ts-ignore
